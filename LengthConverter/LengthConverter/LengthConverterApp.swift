f98ad8dc6584af0bb25974b509d83f6a90f70642
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-10 16:23:01 +0100

    Created Length Converter with the UnitLength class

---
+++ LengthConverter/LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Aleksandar Filipov on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
