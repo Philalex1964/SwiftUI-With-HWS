57a4e22b716e2fc54da010641b848e86cd0feea2
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-14 16:51:10 +0100

    custom button

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,25 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        Button("Delete selection") {
-//            print("Now deleting...")
-//        }
-//        Button("Delete selection", role: .destructive, action: executeDelete)
-//    }
-//
-//    func executeDelete() {
-//        print("Now deleting...")
-//    }
-        VStack {
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.mint)
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
+        Button {
+            print("Button was tapped")
+        } label: {
+            Text("Tap me!")
+                .padding()
+                .foregroundColor(.white)
+                .background(.red)
         }
     }
 }
