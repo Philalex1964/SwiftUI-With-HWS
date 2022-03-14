d1e2c5ce363803139f63fddc0c1e31c5c1299a4b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-14 16:03:26 +0100

    LinearGradient

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,19 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            VStack(spacing: 0) {
-                Color.red
-                Color.blue
-            }
-            
-            Text("Your content")
-//                .foregroundColor(.secondary)
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
-        }
-        .ignoresSafeArea()
+//        LinearGradient(gradient: Gradient(colors: [.white, .blue, .black]), startPoint: .top, endPoint: .bottom)
+//        LinearGradient(gradient: Gradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.45),
+//            Gradient.Stop(color: .black, location: 0.55)
+//        ]), startPoint: .top, endPoint: .bottom)
+        LinearGradient(gradient: Gradient(stops: [
+            .init(color: .white, location: 0.45),
+            .init(color: .black, location: 0.55)
+        ]), startPoint: .top, endPoint: .bottom)
     }
 }
 
