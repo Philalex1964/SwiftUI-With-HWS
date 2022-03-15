23b845711d9b98140c929dce8879bc9ebcb66e1b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-15 12:55:47 +0100

    Set up Project stack butoons

---
+++ GuessTheFlagProject/GuessTheFlagProject/GuessTheFlagProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagProjectApp.swift
+//  GuessTheFlagProject
+//
+//  Created by Aleksandar Filipov on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
