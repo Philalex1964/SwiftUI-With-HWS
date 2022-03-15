8f3a0d5ddce9eac30459329bc6706c79d8824be3
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-15 17:04:28 +0100

    added score, attempts limit, reset

--- GuessTheFlagProject/GuessTheFlagProject/ContentView.swift
+++ GuessTheFlagProject/GuessTheFlagProject/ContentView.swift
@@ -9,20 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var showingScore = false
+    @State private var showingFinalScore = false
     @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
+    @State private var attemptsCount = 0
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
-//            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-//                .ignoresSafeArea()
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.35),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 250, endRadius: 450)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
+//            RadialGradient(stops: [
+//                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+//                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+//            ], center: .top, startRadius: 200, endRadius: 400)
+//                .ignoresSafeArea()
             
             VStack {
                 Spacer()
@@ -60,7 +64,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
+                Text("Score: \(score)")
                     .foregroundColor(.white)
                     .font(.title.bold())
                 
@@ -69,24 +73,51 @@ struct ContentView: View {
             
             .padding()
         }
+        
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text(scoreMessage)
+            Text("Your score is \(score)")
         }
+        
+        .alert(scoreTitle, isPresented: $showingFinalScore) {
+                    Button("Restart", action: reset)
+                } message: {
+                    Text(scoreMessage)
+                    Text("Your final score is \(score)")
+                }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
+            scoreMessage = "Your score is \(score)."
         } else {
             scoreTitle = "Wrong"
+            score -= 1
+            scoreMessage = "That’s the flag of \(countries[number])! Your score is \(score)."
+            
         }
 
-        showingScore = true
+        attemptsCount += 1
+        if attemptsCount == 3 {
+            showingScore = false
+            showingFinalScore = true
+        } else {
+            showingScore = true
+        }
     }
     
     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reset() {
+        score = 0
+        attemptsCount = 0
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
