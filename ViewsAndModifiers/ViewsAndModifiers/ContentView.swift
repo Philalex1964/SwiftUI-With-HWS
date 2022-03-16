3abccf6fdbd8319f9921201adbcb468f8dcb0e7b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-16 14:01:13 +0100

    Views as properties

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,31 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedText = false
-
-       var body: some View {
-//           Envirement modifier can be overrided
-           VStack {
-               Text("Gryffindor")
-                   .font(.largeTitle)
-               Text("Hufflepuff")
-               Text("Ravenclaw")
-               Text("Slytherin")
-           }
-           .font(.title)
-           
-           Spacer()
-           
-//           Regular modifier - can not
-           VStack {
-               Text("Gryffindor")
-                   .blur(radius: 0)
-               Text("Hufflepuff")
-               Text("Ravenclaw")
-               Text("Slytherin")
-           }
-           .blur(radius: 5)
-       }
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    
+    let motto2 = Text("nunquam titillandus")
+//    3 ways to put multiple views in the property
+    
+    var spells: some View {
+        VStack {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+   
+    var spells1: some View {
+        Group {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    
+    @ViewBuilder var spells2: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
+    
+    
+    var body: some View {
+        VStack {
+            motto1
+                .background(.red)
+            motto2
+                .background(.blue)
+            spells
+            spells1
+            spells2
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
