40e0290d112e1e39817c1f349bd44dfa5732a9f6
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-16 15:06:33 +0100

    Custom containers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,54 +7,41 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+//    @ViewBuilder let content: (Int, Int) -> Content
 
-extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) { column in
+                        content(row, column)
+                    }
+                }
+            }
         }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {    
     var body: some View {
-        ZStack {
-            Color.gray
-                .frame(width: 300, height: 200)
-                .watermarked(with: "Hacking with Swift")
-            
-            Text("Hello World!")
-                .titleStyle()
+        GridStack(rows: 4, columns: 4) { row, col in
+            HStack {
+                Image(systemName: "\(row * 4 + col).circle")
+                Text("R\(row) C\(col)")
+                    .frame(width: 50, height: 30)
+            }
         }
+// Use with @ViewBuilder let content: (Int, Int) -> Content
+        /*
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
+         */
     }
 }
 
