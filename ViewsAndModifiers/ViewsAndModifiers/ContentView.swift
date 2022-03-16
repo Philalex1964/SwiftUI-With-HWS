3a65a49beda71a7cc715739db7ac8465023a9ed6
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-16 14:40:04 +0100

    Custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,51 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    
-    var body: some View {
-        VStack(spacing: 10) {
-                    Text("First")
-                        .font(.largeTitle)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(.blue)
-                        .clipShape(Capsule())
-
-                    Text("Second")
-                        .font(.largeTitle)
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(.blue)
-                        .clipShape(Capsule())
-                }
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
-/*
- struct CapsuleText: View {
-     var text: String
 
-     var body: some View {
-         Text(text)
-             .font(.largeTitle)
-             .padding()
-             .foregroundColor(.white)
-             .background(.blue)
-             .clipShape(Capsule())
-     }
- }
- 
- struct ContentView: View {
-     var body: some View {
-         VStack(spacing: 10) {
- CapsuleText(text: "First")
-         .foregroundColor(.white)
-     CapsuleText(text: "Second")
-         .foregroundColor(.yellow)
-         }
-     }
- }
- */
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct ContentView: View {    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Hacking with Swift")
+            
+            Text("Hello World!")
+                .titleStyle()
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
