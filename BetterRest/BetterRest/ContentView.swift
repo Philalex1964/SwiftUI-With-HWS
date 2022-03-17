6e8166fb64ece6756f1df7b7b63927a5947a7de5
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-17 18:01:10 +0100

    Different Date formats

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please, enter a date", selection: $wakeUp, in: Date.now...) //displayedComponents: .hourAndMinute)
-//        .date)
-//            .labelsHidden()
+        Text(Date.now, format: .dateTime.hour().minute())
+        
+        Text(Date.now, format: .dateTime.day().month().year())
+        
+        Text(Date.now.formatted(date: .long, time: .shortened))
+
+        Text(Date.now.formatted(date: .long, time: .omitted))
     }
     
-    func exampleDate() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+    func trivialExample() {
+//        let now = Date.now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = now...tomorrow
+        
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? Date.now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
 }
 
