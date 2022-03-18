71df6a02c9e4c80f9d45d09522fb748848728127
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-18 16:15:52 +0100

    List with arrays

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,28 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-// List w/out ForEach
-        List(0..<5) {
-            Text("Dynamic rows \($0)")
-//  List can generate its rows entirely from dynamic content without needing a ForEach.
+        // Combination
+        List {
+            Text("Static Row")
             
-//            Section("Section 1") {
-//                Text("Static row 1")
-//                Text("Static row 2")
-//            }
-//
-//            Section("Section 2") {
-//                ForEach(0..<7
-//                ) {
-//                    Text("Dynamic rows \($0)")
-//                }
-//            }
-//
-//            Section("Section 3") {
-//                Text("Static row 3")
-//                Text("Static row 4")
-//            }
+            ForEach(people, id: \.self) {
+                Text($0)
+            }
+            
+            Text("Static Row")
+        }
+        
+        // List with arrays
+        List(people, id: \.self) {
+            Text($0)
+            //  List can generate its rows entirely from dynamic content without needing a ForEach.
+            
+            //            Section("Section 1") {
+            //                Text("Static row 1")
+            //                Text("Static row 2")
+            //            }
+            //
+            //            Section("Section 2") {
+            //                ForEach(0..<7
+            //                ) {
+            //                    Text("Dynamic rows \($0)")
+            //                }
+            //            }
+            //
+            //            Section("Section 3") {
+            //                Text("Static row 3")
+            //                Text("Static row 4")
+            //            }
         }
         .listStyle(.grouped)
     }
