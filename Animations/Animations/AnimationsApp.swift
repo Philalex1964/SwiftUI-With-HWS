21ef457f5d7eb2f0770c97290e002d63e0da5120
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-21 13:38:19 +0100

    Set up Animation Project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Aleksandar Filipov on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
