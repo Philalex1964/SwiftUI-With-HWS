402fa512560e973be0c6d8674f546f9de6de50d3
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-21 14:06:15 +0100

    custom animation 2 - overlay

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -12,26 +12,26 @@ struct ContentView: View {
     
     var body: some View {
         Button("Tap me") {
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding()
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .scaleEffect(animationAmount)
-        .blur(radius: (animationAmount - 1) * 3)
-//        .animation(.easeOut, value: animationAmount)
-//        .animation(.easeIn, value: animationAmount)
-//        .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
-//        .animation(.easeInOut(duration: 2)
-//            .delay(1),
-//        value: animationAmount)
-//        .animation(.easeInOut(duration: 1)
-//            .repeatCount(2, autoreverses: true),
-//        value: animationAmount)
-        .animation(.easeInOut(duration: 1)
-            .repeatForever(autoreverses: true),
-        value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        )
+        .onAppear {
+            animationAmount = 2
+        }
     }
 }
 
