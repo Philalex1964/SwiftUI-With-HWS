93a22686d135254a95f00ca9babe21e8332d5daa
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-25 15:05:26 +0100

    onDelete, EditButton

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,36 +7,34 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        Spacer()
-        
-        Text("Hello, \(name)")
-        
-        Spacer()
-        
-        Button("Dismiss") {
-            dismiss()
-        }
-        
-        Spacer()
-    }
-}
-
 struct ContentView: View {
-    @State private var showingSheet = false
-    
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete")
+            .toolbar {
+                EditButton()
+            }
         }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "@twostraws")
-        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
