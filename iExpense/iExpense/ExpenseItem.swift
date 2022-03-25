c5949f30087b975d51e9b7122adb63d05ec687b0
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-25 16:37:02 +0100

    add struct and class. AddButton and onDelete

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  expenseItem.swift
+//  iExpense
+//
+//  Created by Aleksandar Filipov on 3/25/22.
+//
+
+import Foundation
---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Aleksandar Filipov on 3/25/22.
+//
+
+import Foundation
