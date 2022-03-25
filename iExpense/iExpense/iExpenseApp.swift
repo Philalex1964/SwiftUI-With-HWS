2cadf4877ff859e1dfc18f8882d616e937905159
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-25 13:25:22 +0100

    set up new iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Aleksandar Filipov on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
