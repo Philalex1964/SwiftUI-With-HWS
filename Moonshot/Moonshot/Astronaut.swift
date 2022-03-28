b85fa93dee10d0f441008e6066b19e50e3c090fe
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 16:57:10 +0200

    set up a Project. added JSONs and extension for a Bundle.

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,13 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Aleksandar Filipov on 3/28/22.
+//
+
+import Foundation
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,26 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let layout = [
-//        GridItem(.fixed(80)),
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
     
     var body: some View {
-//        ScrollView {
-//            LazyVGrid(columns: layout) {
-//                ForEach(0..<1000) {
-//                    Text("Item \($0)")
-//                }
-//            }
-//        }
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text("Hello, World!")
+            .padding()
     }
 }
 
