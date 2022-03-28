5c023b8fe3f4fc75142d9babf81bf8afaf478a3c
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 15:03:04 +0200

    Resizing images

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Image("Example")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 350, height: 350)
+            .clipped()
     }
 }
 
