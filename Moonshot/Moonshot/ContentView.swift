d99b079dec89846f4994eab335c22244c325c4d5
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 15:33:47 +0200

    scrollView with immediate adding views

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+    }
+
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .scaledToFill()
-                .frame(height: geo.size.height * 0.8)
-                .frame(width: geo.size.width, height: geo.size.height)
-                .clipped()
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth : .infinity)
         }
         
     }
