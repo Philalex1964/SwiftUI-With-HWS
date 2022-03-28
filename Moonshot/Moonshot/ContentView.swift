fc97ff05f09e99703eed5fd02fe7dd95513a1809
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 15:40:05 +0200

    LazyVStack/LazyHStack and ScrollView(.horizontal)

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -22,8 +22,8 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack(spacing: 10) {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
                 ForEach(0..<100) {
                     CustomText("Item \($0)")
                         .font(.title)
