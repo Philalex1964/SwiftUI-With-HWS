c95e2ff80ae2386030519ade172b5a7bd344e7ea
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 16:08:58 +0200

    get data from Codable Data

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct CustomText: View {
     let text: String
 
@@ -22,16 +32,23 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink {
-                    Text("Detail \(row)")
-                } label: {
-                    Text("Hello, world!")
-                        .padding()
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .navigationTitle("SwiftUI")
+            """
+
+            let data = Data(input.utf8)
+            
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                    print(user.address.street)
+            }
         }
     }
 }
