f0840e032aebc7710998998fdfd455094900ac60
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 16:24:05 +0200

    Vertical and horizontal scrolling Grids

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,47 +7,25 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
-struct CustomText: View {
-    let text: String
-
-    var body: some View {
-        Text(text)
-    }
-
-    init(_ text: String) {
-        print("Creating a new CustomText")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
+    let layout = [
+//        GridItem(.fixed(80)),
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
+    
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+//        ScrollView {
+//            LazyVGrid(columns: layout) {
+//                ForEach(0..<1000) {
+//                    Text("Item \($0)")
+//                }
+//            }
+//        }
+        ScrollView(.horizontal) {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
                 }
-            }
-            """
-
-            let data = Data(input.utf8)
-            
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                    print(user.address.street)
             }
         }
     }
@@ -56,5 +34,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
