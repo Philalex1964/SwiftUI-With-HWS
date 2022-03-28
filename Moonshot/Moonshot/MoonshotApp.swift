756365c19f1f6b3ea55451cf9410fb4da9aac3cb
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-28 12:55:34 +0200

    Set up Moonshot project.

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Aleksandar Filipov on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
