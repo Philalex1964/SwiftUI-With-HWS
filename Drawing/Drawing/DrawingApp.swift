e2f492f0e7c69575004b7bb5537ddf1470802c9d
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-29 16:02:07 +0200

    Set up Drawing project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Aleksandar Filipov on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
