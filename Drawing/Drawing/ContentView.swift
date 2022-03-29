f87124a72a0afbea1e76d3280bf5358955a5ffec
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-03-29 16:42:07 +0200

    Circle, stroke and strokeBorder

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -18,41 +18,46 @@ struct Triangle: Shape {
 
         return path
     }
-//    func path(in rect: CGRect) -> Path {
-//        let rotationAdjustment = Angle.degrees(90)
-//        let modifiedStart = startAngle - rotationAdjustment
-//        let modifiedEnd = endAngle - rotationAdjustment
-//
-//        var path = Path()
-//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
-//
-//        return path
-//    }
 }
 
-struct Arc: Shape {
+struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    var insetAmount = 0.0
 
+//    func path(in rect: CGRect) -> Path {
+//        var path = Path()
+//        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+//
+//        return path
+//    }
     func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
 
         return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Triangle()
-//            .fill(.red)
-            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-            .frame(width: 300, height: 300)
+        Circle()
+//            .stroke(.blue, lineWidth: 40)
+            .strokeBorder(.blue, lineWidth: 40)
         
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-            .stroke(.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
     }
 }
 
