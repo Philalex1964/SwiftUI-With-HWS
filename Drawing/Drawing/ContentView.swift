c5fb5c6f3f5d8f91297507e3b6d6b49ca3694ba3
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-04 10:59:30 +0200

    Capsule frame/border

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -10,13 +10,9 @@ import SwiftUI
 struct ContentView: View {
         
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-//            .background(.red)
-//            .border(.red, width: 30)
-//            .background(Image("Example"))
-//            .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
-            .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
