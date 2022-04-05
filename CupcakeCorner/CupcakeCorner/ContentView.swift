f8ae631b3039cd3801eb07bfa19b597139d64f85
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-05 12:08:23 +0200

    resetting

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,28 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
-    
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Creating account…")
-                }
-            }
-//            .disabled(username.isEmpty || email.isEmpty)
-            .disabled(disabledForm)
-        }
-    }
-    
-    var disabledForm: Bool {
-        username.count < 5 || email.count < 5
+        Text("Hello, World!")
+            .padding()
     }
 }
 
