71769a004b7dae6f1f9e6d368eb524d55d5f1ad1
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-06 12:33:40 +0200

    Creating a custom component with @Binding

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Aleksandar Filipov on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
