7975a1145933cf50d0ba89613ce230d0e7bdaac8
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-06 14:44:29 +0200

    clean the project with modified BookwormApp and added DataController

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -12,25 +12,8 @@ struct ContentView: View {
     @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
     var body: some View {
-        VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-            }
-            
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
-        }
+        Text("Hello, World!")
+            .padding()
     }
 }
 
