8e106d6acb6cfcf9d6a2afdaf9ffb48843874866
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-07 15:30:42 +0200

    Created a new CoreDataProject

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Aleksandar Filipov on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
