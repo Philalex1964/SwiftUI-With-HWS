981b0054610bf95804274ba2c32c544fa9ec5681
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-07 15:58:57 +0200

    How does \.self work for ForEach? Hashable.

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -6,14 +6,25 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+struct Student: Hashable {
+    let name: String
 }
 
+struct ContentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+
+    var body: some View {
+        List(students, id: \.self) { student in
+            Text(student.name)
+        }
+        
+//        List {
+//            ForEach([2, 4, 6, 8, 10], id: \.self) {
+//                Text("\($0) is even")
+//            }
+//        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
