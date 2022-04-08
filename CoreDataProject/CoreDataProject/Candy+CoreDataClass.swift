2b9909ff4bc720b9d2b76a1acea660e366e8cf5a
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-08 13:32:42 +0200

    One-to-many relationships with Core Data, SwiftUI, and @FetchRequest

--- CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
+++ CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
@@ -1,5 +1,5 @@
 //
-//  Singer+CoreDataClass.swift
+//  Candy+CoreDataClass.swift
 //  CoreDataProject
 //
 //  Created by Aleksandar Filipov on 4/8/22.
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-@objc(Singer)
-public class Singer: NSManagedObject {
+@objc(Candy)
+public class Candy: NSManagedObject {
 
 }
---
+++ CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Aleksandar Filipov on 4/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
