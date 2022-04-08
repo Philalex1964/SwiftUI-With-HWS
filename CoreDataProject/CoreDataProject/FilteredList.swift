edfd1726d410eaa937ce39264bba46fef26d01fc
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-08 12:02:23 +0200

    add Generics

--- CoreDataProject/CoreDataProject/FilteredList.swift
+++ CoreDataProject/CoreDataProject/FilteredList.swift
@@ -5,6 +5,32 @@
 //  Created by Aleksandar Filipov on 4/8/22.
 //
 
+import CoreData
+import SwiftUI
+
+struct FilteredList<T: NSManagedObject, Content: View>: View {
+    @FetchRequest var fetchRequest: FetchedResults<T>
+
+    // this is our content closure; we'll call this once for each item in the list
+    let content: (T) -> Content
+
+    var body: some View {
+        List(fetchRequest, id: \.self) { item in
+            self.content(item)
+        }
+    }
+
+    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
+        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+        self.content = content
+    }
+}
+
+
+
+
+
+/*
 import SwiftUI
 
 struct FilteredList: View {
@@ -17,8 +43,10 @@ struct FilteredList: View {
     }
     
     init(filter: String) {
-        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter)
-        )
+        _fetchRequest = FetchRequest<Singer>(sortDescriptors: [], predicate: NSPredicate(format: "lastName BEGINSWITH %@", filter))
+//         NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue)
+
     }
 }
+*/
 
