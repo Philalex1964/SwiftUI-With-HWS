8f2f291aea722d27c937862aad13f9e7773005b1
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-08 16:23:10 +0200

    added Guidance

--- Milestone10_12/Milestone10_12/ContentView.swift
+++ Milestone10_12/Milestone10_12/ContentView.swift
@@ -29,6 +29,17 @@ struct ContentView: View {
     }
 }
 
+/*
+
+As very rough guidance, you should:
+
+Aim to use ternary conditional operators rather than use ifconditions.
+Prefer to break large views up into smaller views rather than add complex logic in your view hierarchy.
+Use Group to avoid the 10-view limit, or to add modifier such as navigationTitle() where it would otherwise not be possible.
+Use an explicit @ViewBuilder only for simple properties, but be wary of using it to mask complex logic when really a new view might make more sense.
+Fall back on AnyView if none of the other options would work.
+*/
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
