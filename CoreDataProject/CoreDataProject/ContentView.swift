58e6a6e7bc78f7d327c4c18ecbe92a15e9ed30a7
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-08 11:33:13 +0200

    Dynamically filtering @FetchRequest with SwiftUI

--- CoreDataProject/CoreDataProject/ContentView.swift
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -9,38 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == 'Star Wars'")) var ships: FetchedResults<Ship>
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Trek")) var ships: FetchedResults<Ship>
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "universe IN %@", ["Star Trek", "Aliens", "Firefly", "Star Wars"])) var ships: FetchedResults<Ship>
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH %@", "E")) var ships: FetchedResults<Ship>
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
-//    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
-    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "name Contains %@", "e")) var ships: FetchedResults<Ship>
-//    var compoundPredicate = NSCompoundPredicate(
+    @State private var lastNameFilter = "A"
     
     var body: some View {
-        List(ships, id: \.self) { ship in
-            Text(ship.name ?? "Unknown name")
-        }
-        
-        Button("Add Examples") {
-            let ship1 = Ship(context: moc)
-            ship1.name = "Enterprise"
-            ship1.universe = "Star Trek"
+        VStack {
+            FilteredList(filter: lastNameFilter)
             
-            let ship2 = Ship(context: moc)
-            ship2.name = "Defiant"
-            ship2.universe = "Star Trek"
-            
-            let ship3 = Ship(context: moc)
-            ship3.name = "Millennium Falcon"
-            ship3.universe = "Star Wars"
-            
-            let ship4 = Ship(context: moc)
-            ship4.name = "Executor"
-            ship4.universe = "Star Wars"
-            
-            try? moc.save()
+            Button("Add Examples") {
+                let taylor = Singer(context: moc)
+                taylor.firstName = "Taylor"
+                taylor.lastName = "Swift"
+
+                let ed = Singer(context: moc)
+                ed.firstName = "Ed"
+                ed.lastName = "Sheeran"
+
+                let adele = Singer(context: moc)
+                adele.firstName = "Adele"
+                adele.lastName = "Adkins"
+
+                try? moc.save()
+            }
+
+            Button("Show A") {
+                lastNameFilter = "A"
+            }
+
+            Button("Show S") {
+                lastNameFilter = "S"
+            }
         }
     }
 }
---
+++ CoreDataProject/CoreDataProject/Singer+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Singer+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Aleksandar Filipov on 4/8/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Singer)
+public class Singer: NSManagedObject {
+
+}
