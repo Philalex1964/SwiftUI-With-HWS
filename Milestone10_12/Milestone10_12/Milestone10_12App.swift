255675084513beb3bf01f18b820e9e3a99bb135a
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-08 15:48:05 +0200

    set up

---
+++ Milestone10_12/Milestone10_12/Milestone10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone10_12App.swift
+//  Milestone10_12
+//
+//  Created by Aleksandar Filipov on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Milestone10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
