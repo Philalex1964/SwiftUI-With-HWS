d518d5509df0d3f2748856485f3b9852d6cbb3db
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-04-11 10:43:29 +0200

    added User and Friend structs

---
+++ UsersAndFriends/UsersAndFriends/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  UsersAndFriends
+//
+//  Created by Aleksandar Filipov on 4/11/22.
+//
+
+import Foundation
+
+struct Friend: Codable, Identifiable {
+    let id: String
+    let name: String
+}
