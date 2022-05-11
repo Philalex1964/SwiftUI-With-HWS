ab4039bf82a18d61a6df3d07ad275ef674781855
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-11 11:40:08 +0200

    Property observer and How property wrappers become structs

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var blurAmount = 0.0
+//  Observer works for the button, but doesn't work for the Slider
+//    {
+//        didSet {
+//            print("New value is \(blurAmount)")
+//        }
+//    }
+
+        var body: some View {
+            VStack {
+                Text("Hello, World!")
+                    .blur(radius: blurAmount)
+
+                Slider(value: $blurAmount, in: 0...20)
+
+                Button("Random Blur") {
+                    blurAmount = Double.random(in: 0...20)
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
