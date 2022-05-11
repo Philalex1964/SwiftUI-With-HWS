7f7f5505d42817d85635bcb978c33786c01088aa
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-11 12:22:26 +0200

    cleaned

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,32 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
     
     var body: some View {
         Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-//            .alert("Change background", isPresented: $showingConfirmation) {
-//                Button("Red") { backgroundColor = .red }
-//                Button("Green") { backgroundColor = .green }
-//                Button("Blue") { backgroundColor = .blue }
-//                Button("Cancel", role: .cancel) { }
-//            } message: {
-//                Text("Select a new color")
-//            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+            .padding()
     }
 }
 
