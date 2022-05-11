3333b798842ce3b8300e6ff5051b324b5c52b488
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-11 12:13:48 +0200

    Showing multiple options with confirmationDialog()

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -8,32 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
-//  Observer works for the button, but doesn't work for the Slider
-//    {
-//        didSet {
-//            print("New value is \(blurAmount)")
-//        }
-//    }
-
-        var body: some View {
-            VStack {
-                Text("Hello, World!")
-                    .blur(radius: blurAmount)
-
-                Slider(value: $blurAmount, in: 0...20)
-//                    .onChange(of: blurAmount) { newValue in
-//                        print("New value is \(newValue)")
-//                    }
-
-                Button("Random Blur") {
-                    blurAmount = Double.random(in: 0...20)
-                }
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                showingConfirmation = true
             }
-            .onChange(of: blurAmount) { newValue in
-                print("New value is \(newValue)")
+//            .alert("Change background", isPresented: $showingConfirmation) {
+//                Button("Red") { backgroundColor = .red }
+//                Button("Green") { backgroundColor = .green }
+//                Button("Blue") { backgroundColor = .blue }
+//                Button("Cancel", role: .cancel) { }
+//            } message: {
+//                Text("Select a new color")
+//            }
+            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+                Button("Red") { backgroundColor = .red }
+                Button("Green") { backgroundColor = .green }
+                Button("Blue") { backgroundColor = .blue }
+                Button("Cancel", role: .cancel) { }
+            } message: {
+                Text("Select a new color")
             }
-        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
