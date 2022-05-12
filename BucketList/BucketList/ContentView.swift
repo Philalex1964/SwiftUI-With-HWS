62ed613f9397503c38673aecede52cd6c7bd52ea
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-12 16:12:34 +0200

    Integrating MapKit with SwiftUI

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,52 +5,51 @@
 //  Created by Aleksandar Filipov on 5/12/22.
 //
 
+import MapKit
 import SwiftUI
-enum LoadingState {
-    case loading, success, failed
-}
 
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
-//        if Bool.random() {
-//            Rectangle()
-//        } else {
-//            Circle()
-//        }
-//        if loadingState == .loading {
-//            LoadingView()
-//        } else if loadingState == .success {
-//            SuccessView()
-//        } else if loadingState == .failed {
-//            FailedView()
-//        }
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        /*
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+//            MapMarker(coordinate: location.coordinate)
+            MapAnnotation(coordinate: location.coordinate) {
+                Circle()
+                    .stroke(.red, lineWidth: 3)
+                    .frame(width: 44, height: 44)
+                    .onTapGesture {
+                        print("Tapped on \(location.name)")
+                    }
+//                Text(location.name)
+            }
+        }
+         */
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
     }
 }
