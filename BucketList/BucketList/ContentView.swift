19e7c81565a172c95d61369285304f173636a9f0
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-12 14:24:52 +0200

    Writing data to the documents directory

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
-    
+
 struct ContentView: View {
     var body: some View {
         Text("Hello World")
+            .onTapGesture {
+                let str = "Test message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         
-        
+        // just send back the first one, which ought to be the only one
+        return paths[0]
     }
 }
 
