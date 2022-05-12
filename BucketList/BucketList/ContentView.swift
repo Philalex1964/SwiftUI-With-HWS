8534dcd864af8d0658ad14331c56fedc9d9c81f4
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-12 15:00:22 +0200

    created writeToDocumentDirectory FileManager extension

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -11,26 +11,27 @@ struct ContentView: View {
     var body: some View {
         Text("Hello World")
             .onTapGesture {
-                let str = "Test message"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    try str.write(to: url, atomically: true, encoding: .utf8)
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
+                FileManager.default.writeToDocumentDirectory("Text message", to: "text.txt")
+//                let str = "Test message"
+//                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+//
+//                do {
+//                    try str.write(to: url, atomically: true, encoding: .utf8)
+//                    let input = try String(contentsOf: url)
+//                    print(input)
+//                } catch {
+//                    print(error.localizedDescription)
+//                }
             }
     }
     
-    func getDocumentsDirectory() -> URL {
-        // find all possible documents directories for this user
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        
-        // just send back the first one, which ought to be the only one
-        return paths[0]
-    }
+//    func getDocumentsDirectory() -> URL {
+//        // find all possible documents directories for this user
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//
+//        // just send back the first one, which ought to be the only one
+//        return paths[0]
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -38,3 +39,21 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+extension FileManager {
+    func writeToDocumentDirectory(_ text: String, to file: String) {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        let url = paths[0].appendingPathComponent(file)
+        
+        do {
+            try text.write(to: url, atomically: true, encoding: .utf8)
+            let input = try String(contentsOf: url)
+            print(input)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
+
+
