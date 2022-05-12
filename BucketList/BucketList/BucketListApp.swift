d2d140fe24cd0d0a783cb2a78969096cecbd8c56
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-12 13:45:05 +0200

    set project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Aleksandar Filipov on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
