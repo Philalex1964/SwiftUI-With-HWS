1d41697c7a0a49757bb9eb18379e46163138fb19
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-16 15:59:04 +0200

    added LocationFetcher. TO DO: Finish Challenge

--- UsersWithPhotoAndLocations/UsersWithPhotoAndLocations/LocationFetcherContentView.swift
+++ UsersWithPhotoAndLocations/UsersWithPhotoAndLocations/LocationFetcherContentView.swift
@@ -1,5 +1,5 @@
 //
-//  LocationFetcherView.swift
+//  LocationFetcherContentView.swift
 //  UsersWithPhotoAndLocations
 //
 //  Created by Aleksandar Filipov on 5/16/22.
@@ -7,9 +7,23 @@
 
 import SwiftUI
 
-struct LocationFetcherView: View {
+struct LocationFetcherContentView: View {
+    let locationFetcher = LocationFetcher()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Start Tracking Location") {
+                self.locationFetcher.start()
+            }
+
+            Button("Read Location") {
+                if let location = self.locationFetcher.lastKnownLocation {
+                    print("Your location is \(location)")
+                } else {
+                    print("Your location is unknown")
+                }
+            }
+        }
     }
 }
 
