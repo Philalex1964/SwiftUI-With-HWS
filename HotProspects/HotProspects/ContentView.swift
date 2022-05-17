30bdd9babff7e55a9d668acc47436a30cc3eedc2
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-17 10:27:45 +0200

    .interpolation(.none)

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,50 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
 
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReadings()
-            }
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
-// MARK: - Non Result option
-    func fetchReadingsNonResult() async {
-        do {
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            output = "Found \(readings.count) readings"
-        } catch {
-            print("Download error")
-        }
-    }
-
-// MARK: - Result option
-    func fetchReadings() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-//        do {
-//            output = try result.get()
-//        } catch {
-//            output = "Error: \(error.localizedDescription)"
-//        }
-        switch result {
-            case .success(let str):
-                output = str
-            case .failure(let error):
-                output = "Error: \(error.localizedDescription)"
-        }
-    }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
