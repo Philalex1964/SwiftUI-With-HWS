2b4f7b06f50ed4f995f322e558638b433402f0f9
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-17 11:47:05 +0200

    Scheduling local notifications

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,35 +6,36 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
 
     var body: some View {
-        List {
-            Text("Taylor Swift")
-//  MARK: Swipe actions
-//                .swipeActions {
-//                    Button {
-//                        print("Hi")
-//                    } label: {
-//                        Label("Send message", systemImage: "message")
-//                    }
-//                }
-                .swipeActions {
-                            Button(role: .destructive) {
-                                print("Hi")
-                            } label: {
-                                Label("Delete", systemImage: "minus.circle")
-                            }
-                        }
-                        .swipeActions(edge: .leading) {
-                            Button {
-                                print("Pin")
-                            } label: {
-                                Label("Pin", systemImage: "pin")
-                            }
-                            .tint(.orange)
-                        }
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+
+                // show this notification five seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)            }
         }
     }
 }
