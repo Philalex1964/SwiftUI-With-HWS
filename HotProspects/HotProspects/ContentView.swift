95f413be5b66995cfa72e5845cbd30552496436f
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-17 10:05:43 +0200

    Understanding Swift’s Result type

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -7,34 +7,52 @@
 
 import SwiftUI
 
-
-@MainActor class DelayedUpdater: ObservableObject {
-//    @Published var value = 0
-//    var value = 0
-    var value = 0 {
-        willSet {
-            objectWillChange.send()
-            print(value)
-        }
-    }
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
-    }
-}
-
 struct ContentView: View {
-    @StateObject var updater = DelayedUpdater()
-    
-    var body: some View {
-        Text("Value is: \(updater.value)")
-    }
-}
+    @State private var output = ""
 
+    var body: some View {
+        Text(output)
+            .task {
+                await fetchReadings()
+            }
+    }
+// MARK: - Non Result option
+    func fetchReadingsNonResult() async {
+        do {
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            output = "Found \(readings.count) readings"
+        } catch {
+            print("Download error")
+        }
+    }
+
+// MARK: - Result option
+    func fetchReadings() async {
+        let fetchTask = Task { () -> String in
+            let url = URL(string: "https://hws.dev/readings.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let readings = try JSONDecoder().decode([Double].self, from: data)
+            return "Found \(readings.count) readings"
+        }
+        
+        let result = await fetchTask.result
+        
+//        do {
+//            output = try result.get()
+//        } catch {
+//            output = "Error: \(error.localizedDescription)"
+//        }
+        switch result {
+            case .success(let str):
+                output = str
+            case .failure(let error):
+                output = "Error: \(error.localizedDescription)"
+        }
+    }
+    
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
