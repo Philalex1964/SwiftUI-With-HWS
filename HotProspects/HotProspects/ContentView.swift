95a566bd631b2964c1e8a407467a0af616fa701e
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-17 12:18:34 +0200

    reset the Project

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -6,20 +6,13 @@
 //
 
 import SwiftUI
-import SamplePackage
 
 struct ContentView: View {
     let possibleNumbers = Array(1...60)
     
     var body: some View {
-        Text(results)
-    }
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-
-        return strings.joined(separator: ", ")
+        Text("Hello World")
+            .padding()
     }
 }
 
