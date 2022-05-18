8d2c04382a80e1888af2d1d0521b607ef4da5ba4
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-18 15:13:37 +0200

    Disabling user interactivity with allowsHitTesting()

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -8,96 +8,39 @@
 import CoreHaptics
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//            Text("Hello, World!")
-//                .onTapGesture(perform: simpleSuccess)
-//    }
-//
-//    func simpleSuccess() {
-//        let generator = UINotificationFeedbackGenerator()
-//        generator.notificationOccurred(.warning)
-//    }
-//}
-
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
-    
     var body: some View {
-            Text("Hello, World!")
-            .onAppear(perform: prepareHaptics)
-            .onTapGesture(perform: complexSuccess)
-    }
-
-    func simpleSuccess() {
-        let generator = UINotificationFeedbackGenerator()
-        generator.notificationOccurred(.warning)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
-        }
-    }
-    
-//    func complexSuccess() {
-//        // make sure that the device supports haptics
-//        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-//        var events = [CHHapticEvent]()
+//        ZStack {
+//            Rectangle()
+//                .fill(.blue)
+//                .frame(width: 300, height: 300)
+//                .onTapGesture {
+//                    print("Rectangle tapped!")
+//                }
 //
-//        // create one intense, sharp tap
-//        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-//        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-//        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
-//        events.append(event)
-//
-//        // convert those events into a pattern and play it immediately
-//        do {
-//            let pattern = try CHHapticPattern(events: events, parameters: [])
-//            let player = try engine?.makePlayer(with: pattern)
-//            try player?.start(atTime: 0)
-//            print("Complex success")
-//        } catch {
-//            print("Failed to play pattern: \(error.localizedDescription).")
+//            Circle()
+//                .fill(.red)
+//                .frame(width: 300, height: 300)
+//                .contentShape(Rectangle())
+//                .onTapGesture {
+//                    print("Circle tapped!")
+//                }
+////                .allowsHitTesting(false)
 //        }
-//    }
-    
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-
-        // create one intense, sharp tap
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
         
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
         }
-
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-            print("Complex success")
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription).")
+        .contentShape(Rectangle())
+        .onTapGesture {
+            print("VStack tapped!")
         }
     }
 }
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
