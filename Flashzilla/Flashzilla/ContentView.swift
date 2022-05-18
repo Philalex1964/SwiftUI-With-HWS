3ee1f21a3574349f64336eb1d9a5dcf8d56832f4
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-18 13:16:18 +0200

    Set up a Project

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Aleksandar Filipov on 5/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
