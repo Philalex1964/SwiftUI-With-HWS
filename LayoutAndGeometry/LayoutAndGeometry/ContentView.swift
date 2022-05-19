b47704daf7c08341ad98f6430f0d630a936957cf
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-19 17:12:44 +0200

    How to create a custom alignment guide

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,62 +7,51 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    var body: some View {
-//        //        Text("Live long and prosper")
-//        //            .frame(width: 300, height: 300, alignment: .topLeading)
-//        //            .offset(x: 50, y: 50)
-////        HStack(alignment: .bottom) {
-//        HStack(alignment: .lastTextBaseline) {
-//            Text("Live")
-//                .font(.caption)
-//            Text("long")
-//            Text("and")
-//                .font(.title)
-//            Text("prosper")
-//                .font(.largeTitle)
-//        }
-//    }
-//}
+extension VerticalAlignment {
+//    struct MidAccountAndName: AlignmentID {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in d: ViewDimensions) -> CGFloat {
+            d[.top]
+        }
+    }
 
-//struct ContentView: View {
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            Text("Hello, world!")
-//                .alignmentGuide(.leading) { d in d[.leading] }
-//            Text("This is a longer line of text")
-//        }
-//        .background(.red)
-//        .frame(width: 400, height: 400)
-//        .background(.blue)
-//    }
-//}
-
-//struct ContentView: View {
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            Text("Hello, world!")
-//                .alignmentGuide(.leading) { d in d[.trailing] }
-////                .offset(x: 50, y: 50)
-//            Text("This is a longer line of text")
-//        }
-//        .background(.red)
-//        .frame(width: 400, height: 400)
-//        .background(.blue)
-//    }
-//}
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
 
 struct ContentView: View {
+//    var body: some View {
+//        HStack {
+//            VStack {
+//                Text("@twostraws")
+//                Image("Example")
+//                    .resizable()
+//                    .frame(width: 64, height: 64)
+//            }
+//
+//            VStack {
+//                Text("Full name:")
+//                Text("PAUL HUDSON")
+//                    .font(.largeTitle)
+//            }
+//        }
+//    }
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("@twostraws")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                Image("Example")
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+
+            VStack {
+                Text("Full name:")
+                Text("PAUL HUDSON")
+                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
+                    .font(.largeTitle)
             }
         }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.blue)
     }
 }
 
