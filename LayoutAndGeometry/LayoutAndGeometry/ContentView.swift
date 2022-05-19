ad4cc4c32e97531b86c430b052e76ece7cafe265
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-19 16:48:29 +0200

    Alignment and alignment guides

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,21 +7,62 @@
 
 import SwiftUI
 
+//struct ContentView: View {
+//    var body: some View {
+//        //        Text("Live long and prosper")
+//        //            .frame(width: 300, height: 300, alignment: .topLeading)
+//        //            .offset(x: 50, y: 50)
+////        HStack(alignment: .bottom) {
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+//                .alignmentGuide(.leading) { d in d[.leading] }
+//            Text("This is a longer line of text")
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.blue)
+//    }
+//}
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack(alignment: .leading) {
+//            Text("Hello, world!")
+//                .alignmentGuide(.leading) { d in d[.trailing] }
+////                .offset(x: 50, y: 50)
+//            Text("This is a longer line of text")
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.blue)
+//    }
+//}
+
 struct ContentView: View {
-//    var body: some View {
-//        Text("Hello, world!")
-////            .padding(20)
-//            .background(.red)
-//            .padding(20)
-//    }
-//    var body: some View {
-//        Color.red
-//    }
     var body: some View {
-        Image("Example")
-            .resizable()
-            .frame(width: 300, height: 300)
-            
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
