8aeb5a0e4cbec0edcb1c54f6a88d6bc3afb687b8
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-20 10:06:22 +0200

    Absolute positioning for SwiftUI views

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,51 +7,14 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-//    struct MidAccountAndName: AlignmentID {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
-//    var body: some View {
-//        HStack {
-//            VStack {
-//                Text("@twostraws")
-//                Image("Example")
-//                    .resizable()
-//                    .frame(width: 64, height: 64)
-//            }
-//
-//            VStack {
-//                Text("Full name:")
-//                Text("PAUL HUDSON")
-//                    .font(.largeTitle)
-//            }
-//        }
-//    }
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image("Example")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+//            .background(.red)
+//            .position(x: 100, y: 100)
+//            .offset(x: 100, y: 100)
+            .background(.red)
+            .offset(x: 100, y: 100)
     }
 }
 
