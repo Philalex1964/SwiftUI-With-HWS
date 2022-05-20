8e7fbc1a56bea75b17395ad71c7b226ab94d1f23
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-20 16:55:50 +0200

    Using alert() and sheet() with optionals

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,22 +7,47 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        NavigationView {
-            NavigationLink {
-                Text("New secondary")
-            } label: {
-                Text("Hello, World!")
-            }
-            .navigationTitle("Primary")
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
 
-            Text("Secondary")
-            
-            Text("Tertiary")
-        }
+//struct ContentView: View {
+//    @State private var selectedUser: User? = nil
+//
+//    var body: some View {
+//        Text("Hello, World!")
+//            .onTapGesture {
+//                    selectedUser = User()
+//                }
+//                .sheet(item: $selectedUser) { user in
+//                    Text(user.id)
+//                }
+//    }
+//}
+
+struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    @State private var isShowingUser = false
+
+    var body: some View {
+        Text("Hello, World!")
+            .onTapGesture {
+                selectedUser = User()
+                isShowingUser = true
+            }
+//            .sheet(isPresented: $isShowingUser) {
+//                Text(selectedUser?.id ?? "Unknown")
+//            }
+//            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
+//                Button(user.id) { }
+//            }
+//            .alert("Welcome", isPresented: $isShowingUser) {
+//                Button("OK") { }
+//            }
+            .alert("Welcome", isPresented: $isShowingUser) { }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
