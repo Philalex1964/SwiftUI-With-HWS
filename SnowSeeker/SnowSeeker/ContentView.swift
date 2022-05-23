8f4820156150d37776352652151ca944c56e0ef8
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-23 10:12:59 +0200

    Using groups as transparent layout containers

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,46 +7,59 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.horizontalSizeClass) var sizeClass
+
+    var body: some View {
+//        if sizeClass == .compact {
+//            VStack {
+//                UserView()
+//            }
+//        } else {
+//            HStack {
+//                UserView()
+//            }
+//        }
+        
+        if sizeClass == .compact {
+            VStack(content: UserView.init)
+        } else {
+            HStack(content: UserView.init)
+        }
+    }
 }
 
 //struct ContentView: View {
-//    @State private var selectedUser: User? = nil
+//    @State private var layoutVertically = false
 //
 //    var body: some View {
-//        Text("Hello, World!")
-//            .onTapGesture {
-//                    selectedUser = User()
+//        Group {
+//                    if layoutVertically {
+//                        VStack {
+//                            UserView()
+//                        }
+//                    } else {
+//                        HStack {
+//                            UserView()
+//                        }
+//                    }
 //                }
-//                .sheet(item: $selectedUser) { user in
-//                    Text(user.id)
+//                .onTapGesture {
+//                    layoutVertically.toggle()
 //                }
 //    }
 //}
-
-struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
-
-    var body: some View {
-        Text("Hello, World!")
-            .onTapGesture {
-                selectedUser = User()
-                isShowingUser = true
-            }
-//            .sheet(isPresented: $isShowingUser) {
-//                Text(selectedUser?.id ?? "Unknown")
-//            }
-//            .alert("Welcome", isPresented: $isShowingUser, presenting: selectedUser) { user in
-//                Button(user.id) { }
-//            }
-//            .alert("Welcome", isPresented: $isShowingUser) {
-//                Button("OK") { }
-//            }
-            .alert("Welcome", isPresented: $isShowingUser) { }
-    }
-}
 
 
 struct ContentView_Previews: PreviewProvider {
