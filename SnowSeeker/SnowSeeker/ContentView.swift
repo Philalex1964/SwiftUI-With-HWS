3c70f12873b53d26f3306307bca1400d23c40803
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-23 10:35:04 +0200

    reset

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,40 +7,13 @@
 
 import SwiftUI
 
-//struct ContentView: View {
-//    @State private var searchText = ""
-//
-//    var body: some View {
-//        NavigationView {
-//            Text("Searching for \(searchText)")
-//                .searchable(text: $searchText, prompt: "Look for something")
-//                .navigationTitle("Searching")
-//        }
-//    }
-//}
-
 struct ContentView: View {
     @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
-    
+
     var body: some View {
-        NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
-        }
-    }
+        Text("Hello World")
+            .padding()
     
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-            //            return allNames.filter { $0.contains(searchText)
-            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText)
-            }
-        }
     }
 }
 
