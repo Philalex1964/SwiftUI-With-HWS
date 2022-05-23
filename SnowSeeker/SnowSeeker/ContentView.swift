6d1013412ca9a95156283e63df74574829ae455a
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-23 10:22:57 +0200

    Making a SwiftUI view searchable

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,60 +7,42 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
-
-struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-
-    var body: some View {
-//        if sizeClass == .compact {
-//            VStack {
-//                UserView()
-//            }
-//        } else {
-//            HStack {
-//                UserView()
-//            }
-//        }
-        
-        if sizeClass == .compact {
-            VStack(content: UserView.init)
-        } else {
-            HStack(content: UserView.init)
-        }
-    }
-}
-
 //struct ContentView: View {
-//    @State private var layoutVertically = false
+//    @State private var searchText = ""
 //
 //    var body: some View {
-//        Group {
-//                    if layoutVertically {
-//                        VStack {
-//                            UserView()
-//                        }
-//                    } else {
-//                        HStack {
-//                            UserView()
-//                        }
-//                    }
-//                }
-//                .onTapGesture {
-//                    layoutVertically.toggle()
-//                }
+//        NavigationView {
+//            Text("Searching for \(searchText)")
+//                .searchable(text: $searchText, prompt: "Look for something")
+//                .navigationTitle("Searching")
+//        }
 //    }
 //}
 
+struct ContentView: View {
+    @State private var searchText = ""
+    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    
+    var body: some View {
+        NavigationView {
+            List(filteredNames, id: \.self) { name in
+                Text(name)
+            }
+            .searchable(text: $searchText, prompt: "Look for something")
+            .navigationTitle("Searching")
+        }
+    }
+    
+    var filteredNames: [String] {
+        if searchText.isEmpty {
+            return allNames
+        } else {
+            //            return allNames.filter { $0.contains(searchText)
+            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
