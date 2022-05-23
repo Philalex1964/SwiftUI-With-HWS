2028fe9e8872eaec67e89f6d38c7a4539eb58b4b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-23 15:55:39 +0200

    Binding an alert to an optional string

--- SnowSeeker/SnowSeeker/ResortView.swift
+++ SnowSeeker/SnowSeeker/ResortView.swift
@@ -13,6 +13,9 @@ struct ResortView: View {
     @Environment(\.horizontalSizeClass) var sizeClass
     @Environment(\.dynamicTypeSize) var typeSize
     
+    @State private var selectedFacility: Facility?
+    @State private var showingFacility = false
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 0) {
@@ -30,7 +33,7 @@ struct ResortView: View {
                 }
                 .padding(.vertical)
                 .background(Color.primary.opacity(0.1))
-//                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
+                //                .dynamicTypeSize(...DynamicTypeSize.xxxLarge)
                 
                 Group {
                     Text(resort.description)
@@ -39,15 +42,32 @@ struct ResortView: View {
                     Text("Facilities")
                         .font(.headline)
                     
-//                    Text(resort.facilities.joined(separator: ", "))
-                    Text(resort.facilities, format: .list(type: .and))
-                        .padding(.vertical)
+                    //                    Text(resort.facilities.joined(separator: ", "))
+                    //                    Text(resort.facilities, format: .list(type: .and))
+                    HStack {
+                        ForEach(resort.facilityTypes) { facility in
+                            //                            facility.icon
+                            //                                .font(.title)
+                            Button {
+                                selectedFacility = facility
+                                showingFacility = true
+                            } label: {
+                                facility.icon
+                                    .font(.title)
+                            }
+                        }
+                    }
+                    .padding(.vertical)
                 }
                 .padding(.horizontal)
             }
         }
         .navigationTitle("\(resort.name), \(resort.country)")
         .navigationBarTitleDisplayMode(.inline)
+        .alert(selectedFacility?.name ?? "More information", isPresented: $showingFacility, presenting: selectedFacility) { _ in
+        } message: { facility in
+            Text(facility.description)
+        }
     }
 }
 
