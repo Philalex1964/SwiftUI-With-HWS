d6962929ce303f3274466c98ed98cef415e7744b
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-25 14:55:42 +0200

    SwiftUIView in UIViewController

---
+++ UIHostingControllerSwiftUI/UIHostingControllerSwiftUI/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  UIHostingControllerSwiftUI
+//
+//  Created by Aleksandar Filipov on 5/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Text("Hello, Universe!")
+//                .padding()
+//            
+//            Spacer()
+            
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .padding()
+        }
+        
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
