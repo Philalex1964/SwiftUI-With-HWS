ce4266c600f99c599eabece1bad9a9bccccb83c5
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-26 10:54:30 +0200

    replaced ContentView with LengthConverter

--- UIHostingControllerSwiftUI/UIHostingControllerSwiftUI/ContentView.swift
+++ UIHostingControllerSwiftUI/UIHostingControllerSwiftUI/ContentView.swift
@@ -7,19 +7,96 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-//            Text("Hello, Universe!")
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+////            Text("Hello, Universe!")
+////                .padding()
+////
+////            Spacer()
+//
+//            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
 //                .padding()
-//            
-//            Spacer()
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .padding()
-        }
-        
+//        }
+//
+//
+//    }
+//}
+
+struct ContentView: View {
+    @State private var unitSelected = "m"
+    @State private var inputNumber = "1"
+    @State private var placeHolder = "Input number"
+    @State private var outputUnit = "m"
+    @FocusState private var amountIsFocused: Bool
     
+
+    let units = ["km", "m", "dm", "cm", "mile", "yard", "foot", "inch"]
+    
+    let unitsDictionary = ["km": UnitLength.kilometers, "m": UnitLength.meters, "dm": UnitLength.decimeters, "cm": UnitLength.centimeters, "mile": UnitLength.miles, "yard": UnitLength.yards, "foot": UnitLength.feet, "inch": UnitLength.inches]
+    
+    var outputNumber: Double {
+        let inputMeters = Measurement(value: Double(inputNumber) ?? 1, unit: unitsDictionary[unitSelected] ?? .meters).converted(to: UnitLength.meters)
+        
+        let output = Measurement(value: inputMeters.value, unit: UnitLength.meters) .converted(to: unitsDictionary[outputUnit] ?? .meters)
+        
+        
+        return output.value
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Unit", selection: $unitSelected) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    } .pickerStyle(.segmented)
+                } header: {
+                    Text("Please, choose input unit")
+                }
+                
+                Section {
+                    TextField(placeHolder, text: $inputNumber, onEditingChanged: { editing in
+                        if editing {
+                            inputNumber = ""
+                        }
+                    })
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Please, input number")
+                }
+                
+                Section {
+                    Picker("Unit", selection: $outputUnit) {
+                        ForEach(units, id: \.self) {
+                            Text($0)
+                        }
+                    } .pickerStyle(.segmented)
+                } header: {
+                    Text("Please, choose output unit")
+                }
+                
+                Section {
+                    Text(outputNumber.formatted())
+                } header: {
+                    Text("Your output")
+                }
+            }
+            
+            .navigationTitle("Length Converter")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
