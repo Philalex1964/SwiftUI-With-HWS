f403c36dfb228fd9a9aa384348412f913c246ff9
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-26 10:19:37 +0200

    set Project

---
+++ UIViewControllerRepresentable/UIViewControllerRepresentable/UIViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableApp.swift
+//  UIViewControllerRepresentable
+//
+//  Created by Aleksandar Filipov on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
