c432118a1fd7f7a300a73eaa22b759aaa52a75fb
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-27 10:16:34 +0200

    added signOut

--- AuthenticationStarter/AuthenticationStarter/Views/HomeView.swift
+++ AuthenticationStarter/AuthenticationStarter/Views/HomeView.swift
@@ -11,11 +11,37 @@ import Firebase
 struct HomeView: View {
     
     @EnvironmentObject var viewRouter: ViewRouter
-    
+    @State var signOutProcessing = false
+
     var body: some View {
         NavigationView {
             Text("HomeView")
                 .navigationTitle("V24")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        if signOutProcessing {
+                            ProgressView()
+                        } else {
+                            Button("Sign Out") {
+                                signOutUser()
+                            }
+                        }
+                    }
+                }
+        }
+    }
+    
+    func signOutUser() {
+        signOutProcessing = true
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+            signOutProcessing = false
+        }
+        withAnimation {
+            viewRouter.currentPage = .signInPage
         }
     }
 }
