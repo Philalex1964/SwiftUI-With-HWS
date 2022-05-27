af5fb808a7836aa096cd8278e584a5d53591c52c
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-27 14:50:49 +0200

    added logic and Sign in with Apple button. Need paid Apple development program account to activate ;(

--- AuthenticationStarter/AuthenticationStarter/Views/MotherView.swift
+++ AuthenticationStarter/AuthenticationStarter/Views/MotherView.swift
@@ -6,10 +6,59 @@
 //
 
 import SwiftUI
+//import CryptoKit
+//import FirebaseAuth
+//import AuthenticationServices
 
 struct MotherView: View {
     
     @EnvironmentObject var viewModel: AuthenticationViewModel
+    
+//    @State var currentNonce:String?
+//        
+//        //Hashing function using CryptoKit
+//        func sha256(_ input: String) -> String {
+//            let inputData = Data(input.utf8)
+//            let hashedData = SHA256.hash(data: inputData)
+//            let hashString = hashedData.compactMap {
+//            return String(format: "%02x", $0)
+//            }.joined()
+//
+//            return hashString
+//        }
+//    
+//    // from https://firebase.google.com/docs/auth/ios/apple
+//    private func randomNonceString(length: Int = 32) -> String {
+//      precondition(length > 0)
+//      let charset: Array<Character> =
+//          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+//      var result = ""
+//      var remainingLength = length
+//
+//      while remainingLength > 0 {
+//        let randoms: [UInt8] = (0 ..< 16).map { _ in
+//          var random: UInt8 = 0
+//          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+//          if errorCode != errSecSuccess {
+//            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+//          }
+//          return random
+//        }
+//
+//        randoms.forEach { random in
+//          if remainingLength == 0 {
+//            return
+//          }
+//
+//          if random < charset.count {
+//            result.append(charset[Int(random)])
+//            remainingLength -= 1
+//          }
+//        }
+//      }
+//
+//      return result
+//    }
     
     var body: some View {
         switch viewModel.state {
