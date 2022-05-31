44a44c941f4ba575418eeb7be5df4c5fe9c482d3
aleksandar.filipov <aleksandar.filipov.vegait.rs>
2022-05-31 11:45:07 +0200

    small refactoring in Bookworm

--- Bookworm/Bookworm/RatingView.swift
+++ Bookworm/Bookworm/RatingView.swift
@@ -33,22 +33,9 @@ struct RatingView: View {
                         rating = number
                     }
 //                Approach 1.
-//                    .accessibilityLabel("\(number == 1 ? "1 star" : "\(number) stars")")
-//                    .accessibilityRemoveTraits(.isImage)
-//                    .accessibilityAddTraits(number > rating ? .isButton : [.isButton, .isSelected])
-            }
-        }
-        .accessibilityElement()
-        .accessibilityLabel(label)
-        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
-        .accessibilityAdjustableAction { direction in
-            switch direction {
-            case .increment:
-                if rating < maximumRating { rating += 1 }
-            case .decrement:
-                if rating > 1 { rating -= 1 }
-            default:
-                break
+                    .accessibilityLabel("\(number == 1 ? "1 star" : "\(number) stars")")
+                    .accessibilityRemoveTraits(.isImage)
+                    .accessibilityAddTraits(number > rating ? .isButton : [.isButton, .isSelected])
             }
         }
 //         Approach 2.
